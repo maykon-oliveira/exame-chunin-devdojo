599abc8ff423a38590ad6c979b0d56aab58118b2
Maykon Oliveira <maykonoliveira850@gmail.com>
2020-07-11 22:17:23 -0300

    Criação do projeto

---
+++ src/main/java/com/maykonoliveira/examechunindevdojo/ExameChuninDevdojoApplication.java
@@ -0,0 +1,13 @@
+package com.maykonoliveira.examechunindevdojo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExameChuninDevdojoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExameChuninDevdojoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/maykonoliveira/examechunindevdojo/ExameChuninDevdojoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maykonoliveira.examechunindevdojo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExameChuninDevdojoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
