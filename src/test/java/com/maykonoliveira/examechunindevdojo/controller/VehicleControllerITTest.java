4df7453316f41c8f244d42d26769364d4b9581e3
Maykon Oliveira <maykonoliveira850@gmail.com>
2020-07-13 17:41:02 -0300

    Testes de integração em veiculo controller

--- src/test/java/com/maykonoliveira/examechunindevdojo/controller/VehicleControllerITTest.java
+++ src/test/java/com/maykonoliveira/examechunindevdojo/controller/VehicleControllerITTest.java
@@ -2,13 +2,17 @@ package com.maykonoliveira.examechunindevdojo.controller;
 
 import com.maykonoliveira.examechunindevdojo.entity.Vehicle;
 import com.maykonoliveira.examechunindevdojo.entity.VehicleType;
+import com.maykonoliveira.examechunindevdojo.repository.VehicleRepository;
 import com.maykonoliveira.examechunindevdojo.service.VehicleService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.web.reactive.function.BodyInserters;
@@ -16,32 +20,38 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.math.BigDecimal;
-import java.util.List;
 
-import static org.hamcrest.Matchers.equalTo;
 import static org.mockito.Mockito.when;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 
+@Import(VehicleService.class)
 @ExtendWith(SpringExtension.class)
 @WebFluxTest(VehicleController.class)
-class VehicleControllerTest {
+class VehicleControllerITTest {
   @Autowired private WebTestClient webTestClient;
-  @MockBean private VehicleService vehicleService;
-  private final List<Vehicle> vehicles;
+  @MockBean private VehicleRepository vehicleRepository;
+  private final Vehicle vehicle =
+      Vehicle.builder()
+          .id(1L)
+          .brand("Renault")
+          .type(VehicleType.SPORT)
+          .model("Sport")
+          .year(2020)
+          .price(new BigDecimal(10000))
+          .build();
 
-  VehicleControllerTest() {
-    var vehicle1 = Vehicle.builder().id(1L).brand("Chevrolet").year(2020).build();
-    var vehicle2 = Vehicle.builder().id(2L).brand("BMW").year(2019).build();
-    var vehicle3 = Vehicle.builder().id(3L).brand("Fiat").year(2018).build();
-
-    this.vehicles = List.of(vehicle1, vehicle2, vehicle3);
+  @BeforeEach
+  void setup() {
+    when(vehicleRepository.findAll()).thenReturn(Flux.just(vehicle));
+    when(vehicleRepository.findById(ArgumentMatchers.anyLong())).thenReturn(Mono.just(vehicle));
+    when(vehicleRepository.save(ArgumentMatchers.any(Vehicle.class)))
+        .thenReturn(Mono.just(vehicle));
+    when(vehicleRepository.delete(ArgumentMatchers.any(Vehicle.class))).thenReturn(Mono.empty());
   }
 
   @Test
   @DisplayName("Should returns a list of vehicles with status code 200 and length 3")
   void test1() {
-    when(vehicleService.findAll()).thenReturn(Flux.fromIterable(vehicles));
-
     webTestClient
         .get()
         .uri("/vehicles")
@@ -50,20 +60,15 @@ class VehicleControllerTest {
         .expectStatus()
         .isOk()
         .expectBodyList(Vehicle.class)
-        .value(List::size, equalTo(3));
+        .hasSize(1);
   }
 
   @Test
   @DisplayName("Should returns a vehicle with status code 200 when vehicle exists")
   void test2() {
-    var vehicle = vehicles.get(0);
-    var id = vehicle.getId();
-
-    when(vehicleService.findById(id)).thenReturn(Mono.just(vehicle));
-
     webTestClient
         .get()
-        .uri("/vehicles/" + id)
+        .uri("/vehicles/" + vehicle.getId())
         .accept(APPLICATION_JSON)
         .exchange()
         .expectStatus()
@@ -75,15 +80,11 @@ class VehicleControllerTest {
   @Test
   @DisplayName("Given a invalid vehicle should returns 404 status code")
   void test4() {
-    var vehicle = Vehicle.builder().build();
-
-    when(vehicleService.save(vehicle)).thenReturn(Mono.just(vehicle));
-
     webTestClient
         .post()
         .uri("/vehicles")
         .contentType(APPLICATION_JSON)
-        .body(BodyInserters.fromValue(vehicle))
+        .body(BodyInserters.fromValue(new Vehicle()))
         .exchange()
         .expectStatus()
         .isBadRequest();
@@ -92,17 +93,7 @@ class VehicleControllerTest {
   @Test
   @DisplayName("Given a valid vehicle should save and returns 201 status code")
   void test5() {
-    var price = new BigDecimal(10000);
-    var vehicle =
-        Vehicle.builder()
-            .brand("Renault")
-            .type(VehicleType.SPORT)
-            .model("Sport")
-            .year(2020)
-            .price(price)
-            .build();
-
-    when(vehicleService.save(vehicle))
+    when(vehicleRepository.save(vehicle))
         .thenReturn(
             Mono.just(vehicle)
                 .map(
@@ -128,5 +119,20 @@ class VehicleControllerTest {
         .isEqualTo(VehicleType.SPORT.toString())
         .jsonPath("$.id")
         .isNotEmpty();
+  }
+
+  @Test
+  @DisplayName("Given a id to delete should returns 200 status code")
+  void test6() {
+    webTestClient.delete().uri("/vehicles/" + 1).exchange().expectStatus().isOk();
+  }
+
+  @Test
+  @DisplayName("Given a id that dosen't exist to delete should returns 404")
+  void test7() {
+    var id = 1L;
+    when(vehicleRepository.findById(id)).thenReturn(Mono.empty());
+
+    webTestClient.delete().uri("/vehicles" + id).exchange().expectStatus().isNotFound();
   }
 }
